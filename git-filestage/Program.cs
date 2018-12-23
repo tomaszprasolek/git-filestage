e2f12bbb8684b87a5856e49cf612be75073dd908
Tomasz Prasołek <prasol@o2.pl>
2018-12-23 19:18:06 +0100

    Add LibGit2Sharp nuget

--- git-filestage/Program.cs
+++ git-filestage/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace git_filestage
 {
@@ -7,8 +9,56 @@ namespace git_filestage
     {
         static void Main(string[] args)
         {
+            var repositoryPath = ResolveRepositoryPath();
+            if (string.IsNullOrEmpty(repositoryPath))
+            {
+                Console.WriteLine("fatal: Not a git repository");
+                return;
+            }
+
+            var pathToGit = ResolveGitPath();
+            if (string.IsNullOrEmpty(pathToGit))
+            {
+                Console.WriteLine("fatal: git is not in your path");
+                return;
+            }
+
             var application = new Application();
             application.Run();
+        }
+
+        private static string ResolveRepositoryPath()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var repositoryPath = Repository.Discover(currentDirectory);
+
+            if (string.IsNullOrEmpty(repositoryPath))
+                return null;
+
+            if (!Repository.IsValid(repositoryPath))
+                return null;
+
+            return repositoryPath;
+        }
+
+        private static string ResolveGitPath()
+        {
+            var path = Environment.GetEnvironmentVariable("PATH");
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            var paths = path.Split(Path.PathSeparator);
+
+            // In order to have this work across all operating systems, we
+            // need to include other extensions.
+            //
+            // NOTE: On .NET Core, we should use RuntimeInformation in order
+            //       to limit the extensions based on operating system.
+
+            var fileNames = new[] { "git.exe", "git" };
+            var searchPaths = paths.SelectMany(p => fileNames.Select(f => Path.Combine(p, f)));
+
+            return searchPaths.FirstOrDefault(File.Exists);
         }
     }
 }
