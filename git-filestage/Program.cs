375a66e6c4f3840f14c4651fe0686a46a6e35941
Tomasz Prasołek <kontakt@tomaszprasolek.pl>
2018-12-23 19:09:24 +0100

    Add project files

---
+++ git-filestage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_filestage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
