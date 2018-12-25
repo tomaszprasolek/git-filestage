8c92d846c5c9b9e9a8db4702259f9ef7cd252d29
Tomasz Prasołek <prasol@o2.pl>
2018-12-25 07:15:00 +0100

    Change app initialization, use ConsoleCommands

--- git-filestage/Application.cs
+++ git-filestage/Application.cs
@@ -1,5 +1,6 @@
 ﻿using LibGit2Sharp;
 using System;
+using System.Linq;
 
 namespace git_filestage
 {
@@ -7,6 +8,9 @@ namespace git_filestage
     {
         private string _repositoryPath;
         private string _pathToGit;
+        private bool _done;
+        private ConsoleCommand[] _commands;
+        private int seletedLine = 1;
 
         public Application(string repositoryPath, string pathToGit)
         {
@@ -16,9 +20,50 @@ namespace git_filestage
 
         public void Run()
         {
+            _commands = new[]
+            {
+                new ConsoleCommand(Exit, ConsoleKey.Escape),
+                new ConsoleCommand(Exit, ConsoleKey.Q)
+            };
+
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            InitializeScreen();
+
+            while (_done == false)
+            {
+                var width = Console.WindowWidth;
+                var height = Console.WindowHeight;
+
+                var key = Console.ReadKey(true);
+                var command = _commands.FirstOrDefault(c => c.MatchesKey(key));
+                command?.Execute();
+
+                if (width != Console.WindowWidth || height != Console.WindowHeight)
+                    InitializeScreen();
+            }
+
+            Console.Clear();
+            Console.CursorVisible = true;
+        }
+
+        private void Exit()
+        {
+            _done = true;
+        }
+
+        private void InitializeScreen()
+        {
+            Console.WriteLine("Use arrow keys to select file. Press ENTER to do the action.");
+            Console.WriteLine("1. When file is in working directory, will be added to staging area.");
+            Console.WriteLine("2. When file is in staging area, will be unstaged.");
+            Console.WriteLine("3. When file is not ... TODO");
+            Console.WriteLine("----------");
+
             using (var repo = new Repository(_repositoryPath))
             {
-                foreach (StatusEntry item in repo.RetrieveStatus(new StatusOptions() { Show = StatusShowOption.IndexOnly } ))
+                foreach (StatusEntry item in repo.RetrieveStatus(new StatusOptions() { Show = StatusShowOption.IndexOnly }))
                 {
                     if (item.State == FileStatus.Ignored) continue;
                     WriteFile(item);
@@ -31,10 +76,6 @@ namespace git_filestage
                 }
             }
             Console.ResetColor();
-
-            Console.WriteLine("");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey(true);
         }
 
         private void WriteFile(StatusEntry entry)
